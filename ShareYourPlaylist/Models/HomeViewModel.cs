5ba645590b71e45cea65e282ab5e749439d119ca
sandeepkaranam <sandeepkaranam24@gmail.com>
2024-11-10 23:42:25 -0500

    Add project files.

---
+++ ShareYourPlaylist/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ShareYourPlaylist.Models
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; } = "Welcome to ShareYourPlaylist!";
+        public string WelcomeMessage { get; set; } = "Welcome to ShareYourPlaylist!";
+        public string Description { get; set; } = "Discover and share playlists with your friends!";
+        public string ImageUrl { get; set; } = "/images/welcome-image.jpg"; // Path to a static welcome image
+    }
+}
